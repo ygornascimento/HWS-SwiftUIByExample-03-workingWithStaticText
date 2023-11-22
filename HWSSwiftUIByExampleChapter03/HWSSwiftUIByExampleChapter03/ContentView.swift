831b55039ec52927009a7eb145e0fc144a479bcf
Ygor Nascimento <ygor.nascimento@outlook.com>
2023-11-22 15:34:45 -0300

    How to format text inside text views

--- HWSSwiftUIByExampleChapter03/HWSSwiftUIByExampleChapter03/ContentView.swift
+++ HWSSwiftUIByExampleChapter03/HWSSwiftUIByExampleChapter03/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    let alignments: [TextAlignment] = [.leading, .center, .trailing]
+    @State private var alignment = TextAlignment.leading
+    
     var body: some View {
+
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Picker("Text alignment", selection: $alignment) {
+                ForEach(alignments, id: \.self) { alignment in
+                    Text(String(describing: alignment))
+                }
+            }
+            
+            Text("This is an extremely long text string that will never fit even the widest of phones without wrapping")
+                .font(.largeTitle)
+                .multilineTextAlignment(alignment)
+                .frame(width: 300)
         }
-        .padding()
     }
 }
 
